01d98a7aed39398e2d79e46ffe2428bf8b30cde7
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-03-23 10:47:10 -0400

    added inquirer and initial prompts

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
 
-const printProfileData = profileDataArr => {
-   profileDataArr.forEach(profileItem => console.log(profileItem)); 
-};
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-printProfileData(profileDataArgs);
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
